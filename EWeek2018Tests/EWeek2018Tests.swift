b1c55622e69574ee2172815f52af795b334b4bf3
Aashima Garg <agarg@utexas.edu>
2017-12-26 18:12:29 -0600

    Initial Commit

---
+++ EWeek2018Tests/EWeek2018Tests.swift
@@ -0,0 +1,36 @@
+//
+//  EWeek2018Tests.swift
+//  EWeek2018Tests
+//
+//  Created by Aashima Garg on 12/26/17.
+//  Copyright © 2017 SEC. All rights reserved.
+//
+
+import XCTest
+@testable import EWeek2018
+
+class EWeek2018Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
